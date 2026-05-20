039f15da2e9f446c0c835343d86a59b4d85243e3
Le Thanh Anh <leejay3636@gmail.com>
2026-05-20 15:55:13 +0700

    feat: initialize voice_app package and set thread limits for BLAS libraries

--- voice_app/__init__.py
+++ voice_app/__init__.py
@@ -1,1 +1,8 @@
 __version__ = "0.0.1"
+
+import os
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
