2b0a5f8f5004ac132980639f5662dc06dbe2f1e5
qh.fol <hodacquan2004@gmail.com>
2026-07-07 17:02:34 +0700

    fix(config): update worksuite url to cterp.ctgroupvietnam.com

--- voice_app/constants.py
+++ voice_app/constants.py
@@ -96,7 +96,7 @@ def get_worksuite_url():
             frappe.flags.ignore_permissions = False
             if val: return val
     except Exception: pass
-    return os.getenv("WORKSUITE_URL", "https://erp.ctgroupvietnam.com")
+    return os.getenv("WORKSUITE_URL", "https://cterp.ctgroupvietnam.com")
 
 def get_worksuite_token():
     try:
